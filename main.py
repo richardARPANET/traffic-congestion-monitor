1a5213544b94ed9bfcaf5cd51632dfb7dd624974
richard <richard@richard.do>
2013-06-10 00:06:12 +0100

    lines and points on graph

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import time
 import pygame
 import datetime
 
-point_width = 1
-image_stream = 'http://www.bbc.co.uk/travelnews/motorways/trafficcameras/highwaysagency/50265/1370785351159/image'
-pause_secs = 35  # amount of secs between each graph update, varies according to img stream
+point_width = 2
+image_stream = 'http://streaming2.mdottraffic.com:1935/snapshots?application=rtplive&snap=020607.stream&num=1370817794327'
+pause_secs = 10  # amount of secs between each graph update, varies according to img stream
+time_step = 4 # move this amount on x axis between points
 
 
 def process(r):
@@ -36,23 +37,37 @@ def main():
     pygame.display.set_caption("graph")
     screen.fill((255, 255, 255))
     blue = (0, 0, 255)
+    red = (155, 155, 155)
 
-    i = 0
+    positions = []
+
+    i = 1
     while True:
-        i += point_width
         r = requests.get(image_stream)
+        x_pos = i
         # increase visiblilty
-        value = process(r) * 10
-        print value
-        print datetime.datetime.now()
+        y_pos = process(r) * 10
 
         with open('log.txt', 'a') as f:
-            f.write(str(value) + ',' + str(datetime.datetime.now()) + '\n')
+            f.write(str(y_pos) + ',' + str(datetime.datetime.now()) + '\n')
 
-        pygame.draw.rect(screen, blue, (i, 10, point_width, value), 0)
+        positions.append(y_pos)
+        pygame.draw.rect(screen, blue, (i, y_pos, point_width, point_width), 0)
+
+        prev_y_pos = positions[i/time_step - 1]
+        prev_x_pos = i - time_step
+
+        #surface, color, startpos, endpos
+        if len(positions) == 1:
+            pygame.draw.line(screen, red, (x_pos, y_pos), (x_pos, y_pos))
+        else:
+            pygame.draw.line(screen, red, (prev_x_pos, prev_y_pos), (x_pos, y_pos))
+
+        pygame.transform.flip(screen, 0, 1)
         pygame.display.flip()
         time.sleep(pause_secs)
 
+        i += time_step
 
 if __name__ == '__main__':
     main()
